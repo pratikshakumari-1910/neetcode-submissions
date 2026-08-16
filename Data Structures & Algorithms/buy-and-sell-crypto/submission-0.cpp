5e9d7c128cabbbbd88080801225ecfa08bdc67d6
Pratiksha Kumari <pratikshakumari341@gmail.com>
2026-08-17 00:49:49 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int mn=prices[0];
+        int profit=0;
+        
+        for(int i=0;i<prices.size();i++)
+        {
+            profit = max(profit, prices[i] - mn);
+            mn = min(mn,prices[i]);
+           
+        }
+        return profit;
+    }
+};
---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int hash[256] = {-1};
+        fill(hash, hash + 256, -1);
+
+        int n = s.size();
+        int l=0,r=0, maxlen = 0;
+        while(r<n)
+        {
+            if(hash[s[r]] != -1)
+            {
+                if(hash[s[r]] >= l)
+                {
+                    l = hash[s[r]]+1;
+                }
+            }
+            int len = r-l+1;
+            maxlen = max(len,maxlen);
+            hash[s[r]] = r;
+            r++;
+        }
+        return maxlen;
+    }
+};
